f46f94840c273c03f131908507000661e0560f42
Wira Megantara Pamungkas <89589061+pamungkaswira27@users.noreply.github.com>
2023-12-02 11:14:57 +0700

    feat: implement heal vfx

--- Assets/Game/Code/Scripts/Item Drop/HealingItemSO.cs
+++ Assets/Game/Code/Scripts/Item Drop/HealingItemSO.cs
@@ -5,11 +5,14 @@ namespace ProjectTD
     [CreateAssetMenu(fileName = "Healing Item", menuName = "ProjectTD/Create Item/Healing Item")]
     public class HealingItemSO : ItemSO
     {
+        private const string HEAL_VFX_POOL_TAG = "HealVFX";
+
         [SerializeField]
         private float _healAmount;
 
         public override void Use()
         {
+            ObjectPooler.Instance.GetPooledObject(HEAL_VFX_POOL_TAG, PlayerManager.Instance.Player.transform.position, Quaternion.Euler(-90f, 0f, 0f));
             PlayerManager.Instance.CharacterHealth.IncreaseHealthPoints(_healAmount);
         }
     }
