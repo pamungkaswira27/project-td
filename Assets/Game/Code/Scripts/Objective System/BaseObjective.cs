644c836040ead74660c4919baa7eacd456aa8e39
Wira Megantara Pamungkas <89589061+pamungkaswira27@users.noreply.github.com>
2023-12-08 08:24:17 +0700

    fix: glitch sound

--- Assets/Game/Code/Scripts/Objective System/BaseObjective.cs
+++ Assets/Game/Code/Scripts/Objective System/BaseObjective.cs
@@ -10,7 +10,7 @@ namespace ProjectTD
         public bool HasStarted => _hasStarted;
 
         public virtual void Initialize() { }
-        public virtual void OnStart() { AudioManager.PlaySound(MainSounds.objective_add); }
+        public virtual void OnStart() { }
         public virtual void OnUpdate() { }
         public virtual bool IsObjectiveCompleted() { return false; }
     }
