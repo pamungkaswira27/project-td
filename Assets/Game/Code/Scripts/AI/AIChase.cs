661384f0311a033728adf23d06d870d21c4be2af
Wira Megantara Pamungkas <pamungkaswira27@gmail.com>
2023-12-11 01:02:10 +0700

    fix: weird enemy behaviour

--- Assets/Game/Code/Scripts/AI/AIChase.cs
+++ Assets/Game/Code/Scripts/AI/AIChase.cs
@@ -40,7 +40,7 @@ namespace ProjectTD
             InvokeRepeating(nameof(UpdatePath), 0f, 0.2f);
         }
 
-        private void FixedUpdate()
+        private void Update()
         {
             if (_currentPath != null)
             {
@@ -85,7 +85,7 @@ namespace ProjectTD
             if (!path.error)
             {
                 _currentPath = path;
-                _currentWaypointIndex = 1;
+                _currentWaypointIndex = 0;
             }
         }
 
